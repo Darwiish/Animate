3ede605f419f2a4bfec1e180ad6a076e2a67ee02
abc <abc@example.com>
2019-08-27 18:13:40 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,36 @@
+  $('#').click(function() {
+        $('.').animate({
+            left: "-=40px",
+            fontSize: "+=2px"
+        }, function() {
+            // animation is complete
+        });
+    });
+    
+    $('#').click(function() {
+        $('.').animate({
+            top: "-=40px",
+            opacity: "+=0.1"
+        }, function() {
+            // animation is complete
+        });
+    });
+    
+    $('#').click(function() {
+        $('.').animate({
+            left: "+=40px",
+            fontSize: "-=2px"
+        }, function() {
+            // animation is complete
+        });
+    });
+    
+    $('#').click(function() {
+        $('.').animate({
+            top: "+=40px",
+            opacity: "-=0.1"
+        }, function() {
+            // animation is complete
+        });
+    });
+    
